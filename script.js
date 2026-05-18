b7296cd2066790da11445ff095b85ad5475db8db
darshanrupala9-dotcom <darshanrupala9@gmail.com>
2026-05-19 02:50:56 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,31 @@
+// Minimal smooth animation on scroll
+
+const cards = document.querySelectorAll(
+  '.project-card, .hero-text, .hero-image'
+);
+
+const observer = new IntersectionObserver(entries => {
+
+  entries.forEach(entry => {
+
+    if(entry.isIntersecting){
+
+      entry.target.style.opacity = "1";
+      entry.target.style.transform = "translateY(0px)";
+    }
+
+  });
+
+},{
+  threshold:0.2
+});
+
+cards.forEach(card => {
+
+  card.style.opacity = "0";
+  card.style.transform = "translateY(40px)";
+  card.style.transition = "all 0.8s ease";
+
+  observer.observe(card);
+
+});
